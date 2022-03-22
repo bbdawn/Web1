093db92ec4c78f5aca0aacaa1be823d5c8d748dd
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-22 16:02:33 +0900

    radio, get

---
+++ webstudy05-servlet-basic/src/main/java/step4/RadioServlet.java
@@ -0,0 +1,70 @@
+package step4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RadioServlet
+ */
+@WebServlet("/RadioServlet")
+public class RadioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// client에게 응답하기 위한 문서 타입을 지정(한글 처리 포함)
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body bgcolor=yellow>");
+		out.println(getServletName());
+		
+		//라디오 버튼이 전송한 데이터를 받아온다
+		String info=request.getParameter("gender");
+		out.println("성별: "+info);
+		
+		out.println("</body>");
+		out.println("</html>");
+		out.close();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
