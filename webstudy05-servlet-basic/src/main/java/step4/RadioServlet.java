575cc69a18fcbd8238fbedea23e7c78315c7a1b1
Joohyun Lee <leejoohyun_my@naver.com>
2022-03-24 21:32:46 +0900

    RadioServlet

--- webstudy05-servlet-basic/src/main/java/step4/RadioServlet.java
+++ webstudy05-servlet-basic/src/main/java/step4/RadioServlet.java
@@ -14,44 +14,23 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/RadioServlet")
 public class RadioServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// client에게 응답하기 위한 문서 타입을 지정(한글 처리 포함)
+	private static final long serialVersionUID = 1L;  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//client에게 응답하기 위한 문서 타입을 지정 ( 한글 처리 포함 ) 
 		response.setContentType("text/html;charset=utf-8");
-		PrintWriter out = response.getWriter();
+		PrintWriter out=response.getWriter();
 		out.println("<html>");
 		out.println("<body bgcolor=yellow>");
 		out.println(getServletName());
-		
-		//라디오 버튼이 전송한 데이터를 받아온다
+		// 라디오 버튼이 전송한 데이터를 받아온다 
 		String info=request.getParameter("gender");
-		out.println("성별: "+info);
-		
+		out.println("성별:"+info);
 		out.println("</body>");
 		out.println("</html>");
 		out.close();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
